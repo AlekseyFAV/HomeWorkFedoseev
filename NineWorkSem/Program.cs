5120b9a098f157915fff3cd70f22a4d65b7a79a5
Alex <fedoseev_av91@mail.ru>
2023-07-09 18:57:43 +0300

    task

--- NineWorkSem/Program.cs
+++ NineWorkSem/Program.cs
@@ -120,4 +120,28 @@ int SumNumbers(int start, int end)
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+/*
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{Akkerman(m, n)}");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+*/
